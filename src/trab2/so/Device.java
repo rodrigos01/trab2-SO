483e000320911dc1ab56713705227a7aa58abf64
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-11-04 21:16:47 -0200

    First Commit

---
+++ src/trab2/so/Device.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab2.so;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Device {
+    
+    private static int idCount = 0;
+    
+    private int id, operationTime;
+    private String name;
+
+    public Device(int operationTime, String name) {
+        this.operationTime = operationTime;
+        this.name = name;
+        
+        this.id = ++idCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getOperationTime() {
+        return operationTime;
+    }
+
+    public void setOperationTime(int operationTime) {
+        this.operationTime = operationTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/trab2/so/Process.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab2.so;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Process {
+    
+    private int id, proccessorTime, arrivalTime;
+    private ArrayList<Device> IOOperations;
+    private static int idCount = 0;
+
+    public Process(int proccessorTime, int arrivalTime) {
+        this.proccessorTime = proccessorTime;
+        this.arrivalTime = arrivalTime;
+        this.id = ++idCount;
+        this.IOOperations = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProccessorTime() {
+        return proccessorTime;
+    }
+
+    public void setProccessorTime(int proccessorTime) {
+        this.proccessorTime = proccessorTime;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public ArrayList<Device> getIOOperations() {
+        return IOOperations;
+    }
+
+    public void addIOOperation(Device dev) {
+        this.IOOperations.add(dev);
+    }
+    
+    
+    
+    
+    
+}
